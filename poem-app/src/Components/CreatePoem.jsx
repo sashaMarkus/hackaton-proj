80a9b39cf24b428069d9c1c91499fe63b20f11da
Alexander Markus <sashamarkus@hotmail.com>
2021-06-03 11:40:48 +0300

    added poem submit

--- poem-app/src/Components/CreatePoem.jsx
+++ poem-app/src/Components/CreatePoem.jsx
@@ -1,13 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
+import axios from "axios";
 
 export function CreatePoem() {
+  const [textInput, setTextInput] = useState("");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    const singerAndText = await axios.post("http://localhost:5050/poem", {
+      inputString: textInput,
+      personality: singer
+    })
+    console.log(singerAndText);
+  };
+
   return (
     <div className="d-flex flex-column body">
       <h2>Create a poem</h2>
-      <form className="form-group">
+      <form onSubmit={handleSubmit} className="form-group">
         <p>Insert your words here</p>
-        <input className="form-control" placeholder="Feel poetic yet?" />
-        <button className="btn btn-dark">Create Poem</button>
+        <input
+          className="form-control"
+          placeholder="Feel poetic yet?"
+          value={textInput}
+          onChange={(e) => setTextInput(e.target.value)}
+        />
+        <button type="submit" className="btn btn-dark">
+          Create Poem
+        </button>
       </form>
     </div>
   );
