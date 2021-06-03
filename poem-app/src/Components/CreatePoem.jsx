40eeffeb648d3e2bb77de0ba9c02b98d11572a4f
Alexander Markus <sashamarkus@hotmail.com>
2021-06-03 12:03:02 +0300

    check submit

--- poem-app/src/Components/CreatePoem.jsx
+++ poem-app/src/Components/CreatePoem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import axios from "axios";
 
+
 export function CreatePoem() {
   const [textInput, setTextInput] = useState("");
   const [singer, setSinger] = useState("");
@@ -23,7 +24,7 @@ export function CreatePoem() {
       <h2>Create a poem</h2>
       <form className="form-group">
         <p>Insert your words here</p>
-        <input className="form-control" placeholder="Feel poetic yet?" />
+        <input onChange={ (e) => setTextInput(e.target.value) } className="form-control" placeholder="Feel poetic yet?" />
         <h2>Select artist</h2>
         <p>Move the mouse over the button to open the dropdown menu.</p>
 
@@ -39,7 +40,7 @@ export function CreatePoem() {
           </div>
         </div>
         <h3>Artist Selected: {singer}</h3>
-        <button id="poem-btn" className="btn btn-dark">
+        <button onClick={handleSubmit} id="poem-btn" className="btn btn-dark">
           Create Poem
         </button>
       </form>
